2c1b4c0fa4dfdec84ead05111843d6dab9e17e4c
ralz9 <ralz9-ralz9@mail.ru>
2023-07-18 20:59:46 +0600

    Parser

---
+++ main.py
@@ -0,0 +1,103 @@
+import requests
+import csv
+from bs4 import BeautifulSoup as bs
+
+def write_to(data):
+    with open('data.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow([data['title'], data['pricebucks'], data['price'], data['image'], data['description']])
+        
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+def get_total(html):
+    soup = bs(html, 'lxml')
+    page_list = soup.find('ul', class_="pagination").find_all('a')[-1].attrs.get('data-page')
+    if page_list is not None:
+        return int(page_list)
+    else:
+        return 0
+
+def get_data(html):
+    soup = bs(html, 'lxml')
+    carlist = soup.find('div', class_ = 'table-view-list').find_all('div', class_ = 'list-item list-label')
+
+
+
+    for cars in carlist:
+        try:
+            title = cars.find('div',class_ = "block title").find('h2').text.strip()
+        except AttributeError:
+            title = ''
+
+
+
+
+        try:
+            pricebucks = cars.find('div', class_ = 'price').find('strong').text.split()
+            pricebucks = ' '.join(pricebucks)
+        except AttributeError:
+            pricebucks = ''
+
+
+
+        try:
+            price = cars.find('div', class_ = 'price').find('p').text.replace(pricebucks,'').split()
+            price = ' '.join(price)
+        except AttributeError:
+            price = ''
+
+
+
+        try:
+            image = cars.find('div', class_ = 'thumb-item-carousel').find('img').attrs.get('data-src')
+        except:
+            image = ''
+
+
+
+        try:
+            description = cars.find('div', class_ = 'block info-wrapper item-info-wrapper').text.split()
+            description = ''.join(description)
+        except AttributeError:
+            description = ''
+        
+
+
+        product_dict = {
+                        'title': title, 
+                        'pricebucks' : pricebucks, 
+                        'price' : price, 
+                        'image' : image, 
+                        'description' : description
+                        }
+
+
+
+        write_to(product_dict)
+    
+    
+   
+
+def main():
+    url_code = 'https://www.mashina.kg/search/all/'
+    html = get_html(url_code)
+    get_data(html)
+    number = get_total(html)
+    for i in range(1, number + 1):
+        url_with_page = url_code + '&page=' + str(i)
+        html = get_html(url_with_page)
+        get_data(html)
+    
+
+
+with open('data.csv', 'w') as file:
+   write_ = csv.writer(file)
+   write_.writerow(['title' , 'pricebucks' , 'price' , 'image' , 'description'])
+
+
+
+
+main()
