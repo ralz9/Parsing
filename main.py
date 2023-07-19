342ebd289c71628cf3866878ca847d292f2f4076
ralz9 <ralz9-ralz9@mail.ru>
2023-07-19 17:08:23 +0600

    z

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ def write_to(data):
     with open('data.csv', 'a') as file:
         writer = csv.writer(file)
         writer.writerow([data['title'], data['pricebucks'], data['price'], data['image'], data['description']])
-        
 
 def get_html(url):
     response = requests.get(url)
@@ -22,82 +21,62 @@ def get_total(html):
 
 def get_data(html):
     soup = bs(html, 'lxml')
-    carlist = soup.find('div', class_ = 'table-view-list').find_all('div', class_ = 'list-item list-label')
-
-
+    carlist = soup.find('div', class_='table-view-list').find_all('div', class_='list-item list-label')
 
     for cars in carlist:
         try:
-            title = cars.find('div',class_ = "block title").find('h2').text.strip()
+            title = cars.find('div', class_="block title").find('h2').text.strip()
         except AttributeError:
             title = ''
 
-
-
-
         try:
-            pricebucks = cars.find('div', class_ = 'price').find('strong').text.split()
+            pricebucks = cars.find('div', class_="block price").find('strong').text.split()
             pricebucks = ' '.join(pricebucks)
         except AttributeError:
             pricebucks = ''
 
-
-
         try:
-            price = cars.find('div', class_ = 'price').find('p').text.replace(pricebucks,'').split()
+            price = cars.find('div', class_="block price").find('p').text.replace(pricebucks, '').split()
             price = ' '.join(price)
         except AttributeError:
             price = ''
 
-
-
         try:
-            image = cars.find('div', class_ = 'thumb-item-carousel').find('img').attrs.get('data-src')
+            image = cars.find('div', class_="thumb-item-carousel").find('img').attrs.get('data-src')
         except:
             image = ''
 
-
-
         try:
-            description = cars.find('div', class_ = 'block info-wrapper item-info-wrapper').text.split()
+            description = cars.find('div', class_="block info-wrapper item-info-wrapper").text.split()
             description = ''.join(description)
         except AttributeError:
             description = ''
-        
-
 
         product_dict = {
-                        'title': title, 
-                        'pricebucks' : pricebucks, 
-                        'price' : price, 
-                        'image' : image, 
-                        'description' : description
-                        }
-
-
+            'title': title,
+            'pricebucks': pricebucks,
+            'price': price,
+            'image': image,
+            'description': description
+        }
 
         write_to(product_dict)
-    
-    
-   
+
+        for i in product_dict:
+            f = 'вы успешно спарсили'
+            print(i, f'{f}')
 
 def main():
-    url_code = 'https://www.mashina.kg/search/all/'
-    html = get_html(url_code)
-    get_data(html)
+    base_url = 'https://www.mashina.kg/search/all/'
+    html = get_html(base_url)
     number = get_total(html)
     for i in range(1, number + 1):
-        url_with_page = url_code + '&page=' + str(i)
+        url_with_page = base_url + f'?page={i}'
         html = get_html(url_with_page)
         get_data(html)
-    
-
 
 with open('data.csv', 'w') as file:
-   write_ = csv.writer(file)
-   write_.writerow(['title' , 'pricebucks' , 'price' , 'image' , 'description'])
-
-
-
+   writer = csv.writer(file)
+   writer.writerow(['title', 'pricebucks', 'price', 'image', 'description'])
 
 main()
---
+++ zxc.py
@@ -0,0 +1,8 @@
+def func(a, b):
+    result = a + b
+    return result
+
+counter = 1
+while True:
+    print(func(counter, counter))
+    counter += 1
